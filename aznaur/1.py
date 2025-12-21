18f236046906568bedf85ae86ef2789ff3a0b17a
cat31415 <kirik20052017@gmail.com>
2025-12-21 10:54:13 +0300

    task5_aznaur

--- aznaur/1.py
+++ aznaur/1.py
@@ -5,13 +5,11 @@ txt_path = os.path.join(cur_dir_path, "text.txt")
 
 def count_lines(filename):
     with open(filename, 'r', encoding='utf-8') as f:
-        for count, line in enumerate(f, 1):
-            return count
+        return len(f.readlines())
 
-        
-count_lines("text.txt")  
+def count_words(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        return len(f.read().split())
 
-
-    
-
-        
+print(count_lines(txt_path))
+print(count_words(txt_path))
