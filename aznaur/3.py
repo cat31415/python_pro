c0f50d2574329544c2fad4af46601e748092019f
aznaur778 <aznaur.umataliev@mail.ru>
2026-01-11 21:25:07 +0300

    dz2

--- aznaur/3.py
+++ aznaur/3.py
@@ -12,15 +12,23 @@ class Group:
     def __init__(self, id, name, students, schedule):
         self.id = id
         self.name = name
-        self.students = []
+        self.students = students.copy()
         self.schedule = schedule
 
-    def add_group(self, group):
-        self.groups.append(group)    
+    def add_student_to_group(self, student):
+        if student.group is not None:
+            student.group.remove_student_from_group(student)
+        student.group = self
+        self.students.append(student)
 
+    def remove_student_from_group(self, student):
+        if student in self.students:
+            self.students.remove(student)
+        else:
+            raise ValueError("Student not in that group")
 
-
-
+    def __str__(self):
+        return f"Group(id={self.id}, name={self.name}, students={len(self.students)})"
 
 class Student:
     def __init__(self, id, name, login, password, group):
@@ -31,13 +39,52 @@ class Student:
         self.group = group
 
     def change_group(self, group):
-        self.group = group
+        group.add_student_to_group(self)
 
     def change_password(self, last_password, new_password):
         if self.password == last_password:
             self.password = new_password
-            
+        else:
+            raise ValueError("Incorrect password")
+    
+    def __str__(self):
+        return f"Student(id={self.id}, name={self.name}, login={self.login}, group={self.group.name if self.group else 'None'})"
+
+#Создать класс Teacher(id, name, login, password, subjects) методы change_password(last_password, new_password), add_subject(subject), remove_subject(subject)
+class Teacher:
+    def __init__(self, id, name, login, password, subjects):
+        self.id = id
+        self.name = name
+        self.login = login
+        self.password = password
+        self.subjects = subjects.copy()
         
-       
+    def change_password(self, last_password, new_password):
+        if self.password == last_password:
+            self.password = new_password
+        else:
+            raise ValueError("Incorrect password")
+        
+    def add_subject(self, subject):
+        self.subjects.append(subject)
+
+    def remove_subject(self, subject):
+        if subject in self.subjects:
+            self.subjects.remove(subject)
+
+        
+ 
+        
+    
+
+
+group1 = Group(1, "Group A", [], None)
+student1 = Student(1, "Alice", "alice123", "pass1", None)
+group1.add_student_to_group(student1)
+group2 = Group(2, "Group B", [], None)
+student1.change_group(group2)
+print(student1)
+print(group1)
+print(group2)
 
 
