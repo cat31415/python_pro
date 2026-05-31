c2f82ba8e840334468d761211970c418114d43cf
aznaur <aznaur.umataliev@mail.ru>
2026-05-31 10:12:04 +0300

    7778

--- aznaur/fast/main.py
+++ aznaur/fast/main.py
@@ -3,6 +3,13 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+users = [
+    {"id": 1, "name": "Alice"},
+    {"id": 2, "name": "Bob"},
+    {"id": 3, "name": "Charlie"},
+    {"id": 4, "name": "Alice"},
+]
+
 @app.get("/")
 def home():
     return {"message": "Hello, World!"}
@@ -10,3 +17,18 @@ def home():
 @app.get("/cat")
 def cat():
     return {"message": "Meow!"}
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    for user in users:
+        if user["id"] == user_id:
+            return {"user": user}
+    return {"error": "User not found"}
+
+@app.get("/users/name/{user_name}")
+def get_user(user_name: str): 
+    for user2 in users:
+        if user2["name"] == user_name:
+            return {"user": user2}
+    return {"error": "usres not found"}    
+ 
