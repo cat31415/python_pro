e8f74fecb8d3ffcc23d023588040370ce46d21cb
aznaur <aznaur.umataliev@mail.ru>
2026-06-15 17:36:52 +0300

    errt

--- aznaur/fast/mai.py
+++ aznaur/fast/mai.py
@@ -5,8 +5,8 @@ app = FastAPI()
 # mai:app --reload
 robots = [
     {"id": 0, "name": "Explorer-1"},
-    {"id": 2, "name": "Builder-2"},
-    {"id": 3, "name": "Miner-3"}
+    {"id": 1, "name": "Builder-2"},
+    {"id": 2, "name": "Miner-3"}
 ]
 
 @app.get("/robots",
@@ -19,12 +19,18 @@ def get_rob():
 @app.get("/robots/{rob_id}")
 def get_rob_id(rob_id: int):
     for rob in robots:
-        if rob["id"] == rob_id:
+        if rob["id"] == rob_id: 
             return {"rob": rob}
         
-@app.post("/robots/{new_rob}")
+@app.post("/robots")
 def new_rob(name: str):
-    robots.append(name)
+    id = []
+    for r in robots:
+        id.append(r["id"])
+        
+    max_id = max(id)
+    new_id = max_id + 1
+    robots.append({"id": new_id, "name": name})
     return 
 
 
