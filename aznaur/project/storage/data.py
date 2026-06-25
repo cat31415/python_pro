09dba017654d0dc52dc1c6cd663cf429f96efe10
aznaur778 <aznaur.umataliev@mail.ru>
2026-06-25 07:50:19 +0300

    уенн

--- aznaur/project/storage/data.py
+++ aznaur/project/storage/data.py
@@ -1,1 +1,13 @@
 from models.product import Product
+
+class ProdDB():
+    pass
+
+class ProductBD():
+    product = [
+        Product(id = 1, name = "iPhone 17", pric = 10000),
+        Product(id = 2, name = "Keybord", pric = 2500)
+    ]
+
+    def add_product(self, prod: Product):
+        
